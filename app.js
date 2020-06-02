cee8d6131bbd5488d12799eb8c6f3431e4f67ec3
Jessiya Joy <jessiyajoy@gmail.com>
2020-06-02 20:48:36 +0530

    Navigation Bar included

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const settingsController = require('./controllers/settings');
 const mongoose = require('mongoose');
 const Checklist = require('./models/checklist');
 const checklistRoutes = require('./routes/checklist');
+const path = require('path');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -15,9 +17,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(checklistRoutes);
 
 mongoose
-  .connect(
-    'mongodb+srv://JIPMER:xgIzafJumuLrV0ux@cluster0-opfdu.mongodb.net/journal?retryWrites=true&w=majority'
-  )
-  .then((result) => {
-    app.listen(3000);
-  }).catch(err=>{console.log(err)});
+    .connect(
+        'mongodb+srv://JIPMER:xgIzafJumuLrV0ux@cluster0-opfdu.mongodb.net/journal?retryWrites=true&w=majority'
+    )
+    .then((result) => {
+        app.listen(3000);
+    }).catch(err => { console.log(err) });
