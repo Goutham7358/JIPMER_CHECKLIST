8d69fddeb1653a95c5c9c641b8ff4ad4adace70b
Goutham7358 <gouthampoloth@gmail.com>
2020-06-05 14:02:55 +0530

    Edit Option Item Number Change Functionality Added
    -Created Static methods inside Checklist Model
    -New Edit Controller added

--- models/checklist.js
+++ models/checklist.js
@@ -12,8 +12,90 @@ const checkListSchema  = new Schema({
     }
 });
 
-checkListSchema.statics.getCheckedItems = function(result){
+checkListSchema.statics.getSortedItems = async function (){
+
+    const items = await this.find();
+    function compare(a, b) {
+        if (a.number < b.number) {
+            return -1;
+        } else if (a.number > b.number) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    items.sort(compare);
+
+    return items;
    
+}
+
+checkListSchema.statics.addItemInOrder = async function(items, itemNumber, itemDescription){
+    async function asyncForEach(array, callback) {
+        for (let index = itemNumber - 1; index < array.length; index++) {
+            await callback(array[index], index, array);
+        }
+    }
+
+    const itemUpdate = async (item, index, items) => {
+        item.number = index + 2;
+       await item.save();
+    };
+
+    const start = async() => {
+        await asyncForEach(items, itemUpdate);
+    };
+
+    if (itemNumber > items.length + 1) {
+        itemNumber = items.length + 1;
+        console.log("This can't be done?", itemNumber)
+        const checklist = new this({
+            number: itemNumber,
+            description: itemDescription,
+        });
+        (async()=>{
+            await  checklist.save()
+        })();
+     
+    } 
+     if (itemNumber >= 0) {
+            await start()
+            const checklist = new this({
+                number: itemNumber,
+                description: itemDescription,
+            });
+
+            (async()=>{
+                await  checklist.save()
+            })();
+    
+    }
+}
+
+checkListSchema.statics.deleteItemInOrder = async function(deleteId,deleteNumber){
+
+    let items;
+
+    async function asyncForEach(array, callback) {
+        for (let index = 0; index < array.length; index++) {
+            await callback(array[index]);
+        }
+    }
+
+    const start = async() => {
+        await asyncForEach(items, async(item) => {
+            if (item.number > deleteNumber) {
+                item.number--;
+                await item.save();
+            }
+        });
+
+    }
+
+    await this.findByIdAndRemove(deleteId)
+     items =  await this.find()
+    await start()
 }
 
 module.exports = mongoose.model('Checklist',checkListSchema);
