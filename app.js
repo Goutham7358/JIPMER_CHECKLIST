3a04eb835d476701939e62dab2723984bf18986f
gokulsreekumar <gokulsreekumar2@gmail.com>
2020-06-23 21:56:35 +0530

    CSRF Working, WithSome error
    - csrf in all screens placed
    - csrf working, but add point not working

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const path = require('path');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const flash = require('connect-flash');
-const csrf = require('csurf')
+const csrf = require('csurf');
+const User = require('./models/user');
 
 
 const MONGODB_URI = 'mongodb+srv://JIPMER:xgIzafJumuLrV0ux@cluster0-opfdu.mongodb.net/journal'
@@ -26,6 +27,12 @@ const store = new MongoDBStore({
 
 const csrfProtection = csrf();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Middleware for initialzing session with store:
 
 app.use(
@@ -36,13 +43,22 @@ app.use(
     store: store
     })
 );
+
 app.use(csrfProtection);
 app.use(flash());
-app.set('view engine', 'ejs');
-app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+    if(!req.session.user){
+        return next();
+    }
+    User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+})
+
 app.use((req,res,next) => {
     let token;
     res.locals.csrfToken = token = req.csrfToken();
