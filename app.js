40b08d7c7c984ab34ee6068df0567ae71b7402d7
Goutham7358 <gouthampoloth@gmail.com>
2020-06-30 17:23:53 +0530

    Minor UI Additions

--- app.js
+++ app.js
@@ -101,12 +101,5 @@ app.use((error, req, res, next) => {
 
 mongoose.connect(MONGODB_URI)
     .then((result) => {
-<<<<<<< HEAD
-        app.listen(3000);
-    }).catch(err => {
-        console.log(err);
-    });
-=======
         app.listen(process.env.PORT || 3000);
     }).catch(err => { console.log(err) });
->>>>>>> Making the app production ready
